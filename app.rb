f173a886d275cdca196ee4eaccd14e37e2e79932
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-03 10:54:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,4 +21,9 @@ class App < Sinatra::Base
     @total_phrase
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @phrase = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+    @phrase
+  end
+
 end
