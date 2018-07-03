c444e1a615bebf7c81073ca739268cd6c9fe3a43
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-03 10:46:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     @number = params[:number].to_i
     @number.times do
-      @phrase
+      puts @phrase
     end
     binding.pry
   end
